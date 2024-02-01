a5f466243818033889f9c8e1c746c86ae3c8082e
KatayR <rasitarslan@outlook.com>
2024-02-01 22:55:18 +0300

    base

---
+++ android/app/src/main/kotlin/com/example/first_test_case/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_test_case
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
